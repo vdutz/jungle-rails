da0e0fb9166a0dc181a600756bc5f449709688b5
Vlad Duta <vlad.du09@gmail.com>
2017-08-03 20:39:55 +0000

    Added display of order items for succesful order

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :email, presence: true,
+                    uniqueness: true
+  validates :name, presence: true
+  validates :password_digest, presence: true
+
 end
