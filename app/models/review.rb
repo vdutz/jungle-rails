f825b003fab6195da83f7f5e45708572678581b1
Vlad Duta <vlad.du09@gmail.com>
2017-08-04 15:39:32 +0000

    Added validations to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true,
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 1,
+                                    less_than_or_equal_to: 5}
+
 end
